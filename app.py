a0f36a2e4f0f4fe3a9ffa35fe2b110b1190a877f
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-10-30 21:25:14 +0700

    set up web server

--- app.py
+++ app.py
@@ -1,100 +1,31 @@
 from flask import Flask, request, jsonify
-import os
-import pandas as pd
-from keras_preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow import keras
+
+
+from run_model import run_model
 
 app = Flask(__name__)
 
-def run_model(route_id, direction_id, future_time):
-    # Load the pre-trained model
-    model = keras.models.load_model(os.path.abspath(os.path.join("model", "LSTM_1_model_saved_model")))
-    # Input dataset to model
-    main_data = pd.read_csv(os.path.join("dataset", "pre_processed_data.csv"))
-    filter_by_route = main_data['route_id'] == route_id
-    filter_by_direction = main_data['direction_id'] == direction_id
-    main_data = main_data[filter_by_direction & filter_by_route]
-    main_data = main_data.sort_values(by=['trip_id','stop_sequence_x'])
+@app.route('/predict_congestion', methods=['POST'])
+def predict_congestion():
+    # Assuming the data is sent as JSON in the request
+    request_data = request.get_json()
 
+    # Extract 'route_id', 'direction_id', and 'future_time' from the request
+    route_id = request_data.get('route_id')
+    direction_id = request_data.get('direction_id')
+    future_time = request_data.get('future_time')
 
-    # Update the first row of 'arrival_time' with the provided future_time
-    main_data.loc[main_data.index[0], 'arrival_time'] = pd.to_datetime(future_time)
+    # Your existing script here
+    # ...
 
-    calculated_arrival_times = []
+    # Replace this with your script's logic
+    # Load the pre-trained model and process the data
+    result_data = run_model(route_id, direction_id, future_time)
 
-    for idx in range(len(main_data)):
-        if idx < 1:
-            # For the first row, we've already updated it, so use the provided future_time
-            calculated_arrival_times.append(pd.to_datetime(future_time))
-        else:
-            # For subsequent rows, calculate the 'arrival_time' based on the two previous rows
-            previous_arrival_time = calculated_arrival_times[idx - 1]
-            previous_runtime = main_data['runtime_sec'].iloc[idx - 1]
-            new_arrival_time = previous_arrival_time + pd.to_timedelta(previous_runtime, unit='s')
-            calculated_arrival_times.append(new_arrival_time)
+    # Convert the result data to JSON
+    result_json = result_data.to_json(orient='records')
 
-    # Update the 'arrival_time' column with the calculated values
-    main_data['arrival_time'] = calculated_arrival_times
-
-    # -- Create 'arrival_hour' and 'arrival_minute' --
-    main_data['arrival_hour'] = main_data['arrival_time'].dt.hour
-    main_data['arrival_minute'] = main_data['arrival_time'].dt.minute
-
-    main_data = main_data[
-        ['trip_id', 'start_stop_id', 'arrival_time', 'arrival_hour', 'arrival_minute', 'stop_sequence_x', 'stop_lat',
-         'stop_lon', 'route_id', 'direction_id', 'speed_kmh', 'segment_max_speed_kmh', 'runtime_sec', 'end_stop_id',
-         'distance_m', 'next_lat', 'next_lon', 'congestion_level']]
-
-
-    # Ensure the new data is preprocessed in the same way as the training data
-    scaler = MinMaxScaler()
-    X_new = main_data[ ['arrival_hour', 'arrival_minute', "start_stop_id", 'stop_lat', 'stop_lon', "end_stop_id", 'next_lat', 'next_lon',
-     'direction_id']]
-    X_new = scaler.fit_transform(X_new)
-
-
-    # Define the sequence length (seq_length) used during training
-    seq_length = 64
-
-    # Generate sequences from the new data
-    X_seq_new = []
-    num_objects = len(X_new)
-
-    for i in range(num_objects):
-        start = max(0, i - seq_length + 1)
-        sequence = X_new[start:i + 1]
-        X_seq_new.append(sequence)
-
-    # Pad sequences to ensure consistent length
-    X_seq_new = pad_sequences(X_seq_new, maxlen=seq_length, dtype='float32', padding='post', truncating='post')
-
-    # Assuming 'arrival_time' is the column in the 'test' DataFrame representing the time steps
-    timestamps = main_data['arrival_time'].iloc[seq_length - 1:num_objects]
-
-    # Make predictions
-    predicted_congestion = model.predict(X_seq_new)
-
-    main_data["congestion_level"] = predicted_congestion
-
-    return main_data[["stop_lat","stop_lon","congestion_level"]]
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-
-    # Extract parameters from the JSON request
-    route_id = data['route_id']
-    direction_id = data['direction_id']
-    future_time = data['future_time']
-
-    # Call the run_model function to get predictions
-    result = run_model(route_id, direction_id, future_time)
-
-    # Convert the result to JSON
-    response = result.to_json(orient='records')
-
-    return jsonify(response)
+    return jsonify(result_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- request.py
+++ request.py
@@ -1,23 +1,17 @@
 import requests
-import json
-import pprint
 
-# Define the URL of your Flask server
-server_url = "http://127.0.0.1:5000"  # Change this to your server's URL
+# Set the URL of your Flask server
+url = "http://127.0.0.1:5000/predict_congestion"
 
-# Define the input parameters
+# Define the data to send in the request
 data = {
-    "route_id": 300025,
-    "direction_id": 0,
+    "route_id": 1700960,
+    "direction_id": 1,
     "future_time": "2023-10-25 23:50:00"
 }
 
-# Make a POST request to the /predict route
-response = requests.post(f"{server_url}/predict", json=data)
+# Send the POST request
+response = requests.post(url, json=data)
 
-if response.status_code == 200:
-    result = response.json()
-    # Format the JSON response with an indentation of 4 spaces
-    pprint.pprint(result, compact=True)
-else:
-    print(f"Error: {response.status_code} - {response.text}")
+# Extract and print the JSON response
+print(response.json())
